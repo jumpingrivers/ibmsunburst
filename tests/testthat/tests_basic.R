a8c54343c5120a0cdacb281ca6a69eef2be588e1
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-10-02 15:43:18 +0200

    tests

--- tests/testthat/tests_basic.R
+++ tests/testthat/tests_basic.R
@@ -10,8 +10,20 @@ test_that("handles arguments as expected", {
   expect_error(ibmsunburst())
 
   # works with file path as our data source
+  json_path <- system.file(
+    "extdata", "profiles",
+    "en_v2.json", package = "ibmsunburst"
+  )
+
+  sunviz <- ibmsunburst(json = json_path, version = "v2")
+  expect_is(sunviz, "ibmsunburst")
+  expect_is(sunviz, "htmlwidget")
 
   # works with character json as our data source
+  sunviz2 <- ibmsunburst(json = jsonlite::read_json(json_path),
+                         version = "v2")
+  expect_is(sunviz2, "ibmsunburst")
+  expect_is(sunviz2, "htmlwidget")
 
   # works with R list as our data source
 
